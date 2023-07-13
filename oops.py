6b91cfbb41cf594d86e68cfc8997bbef2cbf9821
Vijaya Lavanya S <135433338+vijayalavanya01@users.noreply.github.com>
2023-07-13 12:21:08 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,76 @@
+class Multiple():
+    
+    
+    def Subfields():
+        print("Sub-fields in AI are:\n")
+        print("Machine Learning\n")
+        print("Neural Network\n")
+        print("Vision\n")
+        print("Robotics\n")
+        print("Speech Processing\n")
+        print("Natural Language Processing\n")
+        
+        
+        
+        
+    def OddEven():
+              
+        num3 = int(input("enter the number"))
+        if(num3%2 == 0):
+            print("The given number is Even")
+            message = "The given number is Even"
+            return message
+        else:
+            print("The given number is odd")
+            message = "The given number is odd"
+            return message
+        
+    def Elegible():
+        
+        gender = input("enter the gender")
+        age = int(input("enter the age"))
+        print("Your Gender:", gender)
+        if age >18:
+            print("Eligible")
+        else:
+            print("Not Eligible")
+            
+            def percentage():
+                                
+                num1 = int(input("Enter the mark1"))
+                num2 = int(input("Enter the mark2"))
+                num3 = int(input("Enter the mark3"))
+                num4 = int(input("Enter the mark4"))
+                num5 = int(input("Enter the mark5"))
+                sum = num1+num2+num3+num4+num5
+                percentage =sum/5
+                print("Total Marks",sum)
+                print(percentage)
+            
+    def triangle():
+        num1 = float(input("Enter the Height"))
+        num2 = float(input("Enter the Breadth"))
+        num3 = float(input("Enter the Height1"))
+        num4 = float(input("Enter the Height2"))
+        num5 = float(input("Enter the Breadth2"))
+        Area = float((num1*num2)/2)
+        perimeter = float(num3+num4+num5)
+        print("Height:",num1)
+        print("Breadth : ",num2)
+        print("Area formula: (Height*Breadth)/2")
+        print("Area of Triangle:",Area)
+        print("Height1 :",num3)
+        print("Height2 :",num4)
+        print("Breadth :",num5)
+        print("Perimeter formula: Height1+Height2+Breadth")
+        print("Perimeter of Triangle: ",perimeter)
+            
+        
+    
+            
+                
+
+
+        
+    
+        
